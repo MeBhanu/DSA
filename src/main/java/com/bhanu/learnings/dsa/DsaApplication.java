6c0075223d0fdc55f1e4d363704983647923fcb3
MeBhanu <bhanurana.44@gmail.com>
2024-12-13 02:13:15 +0530

    project dummy for dsa learnings

---
+++ src/main/java/com/bhanu/learnings/dsa/DsaApplication.java
@@ -0,0 +1,13 @@
+package com.bhanu.learnings.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaApplication.class, args);
+	}
+
+}
